f7ccf5d35492670098d89ad21085df7e119625d5
lxy <778720543@qq.com>
2018-08-24 22:40:59 +0800

    pojo.Category.java增加了note

--- src/main/java/com/how2java/tmall/pojo/Category.java
+++ src/main/java/com/how2java/tmall/pojo/Category.java
@@ -3,6 +3,7 @@ package com.how2java.tmall.pojo;
 public class Category {
     private Integer id;
     private String name;
+    private String note;
 
     public Integer getId() {
         return id;
@@ -20,4 +21,11 @@ public class Category {
         this.name = (name == null ? null : name.trim());//name.trim()去掉name两端的空白字符
     }
 
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
 }
