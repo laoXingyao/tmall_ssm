de9fab10ffb392ec4c08ba937e183e5c872cd11d
lxy <778720543@qq.com>
2018-08-24 22:11:46 +0800

    src/main/java/com/how2java/tmall/pojo/Category.java

--- src/main/java/com/how2java/tmall/pojo/Category.java
+++ src/main/java/com/how2java/tmall/pojo/Category.java
@@ -3,7 +3,31 @@ package com.how2java.tmall.pojo;
 public class Category {
 
     private Integer id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
     private String name;
     private String note;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
