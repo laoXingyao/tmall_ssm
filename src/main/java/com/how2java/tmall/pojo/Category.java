47f53d539a2878131632e5eb79e64a3fcdc08be3
lxy <778720543@qq.com>
2018-08-24 22:15:53 +0800

    a lit change

--- src/main/java/com/how2java/tmall/pojo/Category.java
+++ src/main/java/com/how2java/tmall/pojo/Category.java
@@ -3,19 +3,6 @@ package com.how2java.tmall.pojo;
 public class Category {
 
     private Integer id;
-
-    public String getName() {
-        return name;
     }
 
-    private String name;
-    private String note;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-}
