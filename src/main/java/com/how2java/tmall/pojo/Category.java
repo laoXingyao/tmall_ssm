91dfda248989fa0d1ba34a207378ba417a4a9a8e
laoXingyao <778720543@qq.com>
2018-08-24 22:15:20 +0800

    Update Category.java

--- src/main/java/com/how2java/tmall/pojo/Category.java
+++ src/main/java/com/how2java/tmall/pojo/Category.java
@@ -8,18 +8,6 @@ public class Category {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getNote() {
-        return note;
-    }
-
-    public void setNote(String note) {
-        this.note = note;
-    }
-
     private String name;
     private String note;
 
